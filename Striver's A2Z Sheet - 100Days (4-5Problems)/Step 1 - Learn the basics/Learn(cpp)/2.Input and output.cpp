4ec8050d19a3db268bb769b5597369e2f9d62955
Sane Aalam <saneraza78692@gmail.com>
2025-02-07 00:38:36 +0530

    feat: recall basic

---
+++ Striver's A2Z Sheet - 100Days (4-5Problems)/Step 1 - Learn the basics/Learn(cpp)/2.Input and output.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+    int number;
+    cout << "Enter the number : " << endl;
+    cin >> number;
+    cout << "Print the number : " << number << endl;
+	return 0;
+
+}
